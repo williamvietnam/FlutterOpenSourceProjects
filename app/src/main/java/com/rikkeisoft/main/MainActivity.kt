f39fc42b79393248ecbddb21f5111fff20257410
NguyenBangGiang <giangnb2@rikkeisoft.com>
2022-05-23 14:43:51 +0700

    first commit branch main

---
+++ app/src/main/java/com/rikkeisoft/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rikkeisoft.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rikkeisoft.main.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
