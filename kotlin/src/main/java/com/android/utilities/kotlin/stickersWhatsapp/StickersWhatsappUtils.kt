6e3ff729b6552ac30910f18a98dd2933da3f4b31
NguyenBangGiang <williamnb.vn@gmail.com>
2023-05-26 23:42:13 +0700

    kotlin: init sticker utils

---
+++ kotlin/src/main/java/com/android/utilities/kotlin/stickersWhatsapp/StickersWhatsappUtils.kt
@@ -0,0 +1,5 @@
+package com.android.utilities.kotlin.stickersWhatsapp
+
+class StickersWhatsappUtils{
+
+}
---
+++ kotlin/src/main/java/com/android/utilities/kotlin/stickersWhatsapp/base/BaseStickerFragment.kt
@@ -0,0 +1,6 @@
+package com.android.utilities.kotlin.stickersWhatsapp.base
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseStickerFragment : Fragment() {
+}
